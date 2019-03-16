327adb35801db97602467ff3543acb6a3a2bf288
segur <segur.opus@gmail.com>
2019-03-17 01:37:26 +0900

    Set multiple buttons

---
+++ ZenjectPractice/Assets/ZenjectPractice/Scripts/MultiButtonObserverDI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+[RequireComponent(typeof(Renderer))]
+public class MultiButtonObserverDI : MonoBehaviour
+{
+    [Inject]
+    List<Button> buttons;
+
+    Renderer targetRenderer;
+
+    void Start()
+    {
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(OnClick);
+        }
+        targetRenderer = GetComponent<Renderer>();
+    }
+
+    void OnClick()
+    {
+        targetRenderer.enabled = true;
+    }
+}
