c4efbf77de323cd0a869010f07660969b04acae8
segur <segur.opus@gmail.com>
2019-03-16 22:31:34 +0900

    Create a script for DI

---
+++ ZenjectPractice/Assets/ZenjectPractice/Scripts/ButtonObserverDI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+[RequireComponent(typeof(Renderer))]
+public class ButtonObserverDI : MonoBehaviour
+{
+    [Inject]
+    Button button;
+
+    Renderer targetRenderer;
+
+    void Start()
+    {
+        button.onClick.AddListener(OnClick);
+        targetRenderer = GetComponent<Renderer>();
+    }
+
+    void OnClick()
+    {
+        targetRenderer.enabled = true;
+    }
+}
