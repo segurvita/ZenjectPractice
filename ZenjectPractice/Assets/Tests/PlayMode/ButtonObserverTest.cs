7f4f104a19b0cf001999b810dc4f756242b83d87
segur <segur.opus@gmail.com>
2019-03-24 00:38:16 +0900

    Create test code for ButtonObserver

---
+++ ZenjectPractice/Assets/Tests/PlayMode/ButtonObserverTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ButtonObserverTest
+{
+    ButtonObserver buttonObserver;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        var gameObject = new GameObject("Test");
+        gameObject.AddComponent<MeshRenderer>();
+        buttonObserver = gameObject.AddComponent<ButtonObserver>();
+    }
+
+    [UnityTest]
+    public IEnumerator TestOnClick()
+    {
+        buttonObserver.targetRenderer.enabled = false;
+        Assert.AreEqual(false, buttonObserver.targetRenderer.enabled);
+
+        buttonObserver.OnClick();
+        Assert.AreEqual(true, buttonObserver.targetRenderer.enabled);
+
+        yield return null;
+    }
+}
